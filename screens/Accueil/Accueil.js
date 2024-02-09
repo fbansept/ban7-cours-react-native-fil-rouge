1bee7e2f695ecab8d7b3670085176fcd28475fc6
fbansept <bansept.franck@gmail.com>
2024-02-09 12:07:39 +0100

    get offres

--- screens/Accueil/Accueil.js
+++ screens/Accueil/Accueil.js
@@ -4,42 +4,66 @@ import { FlatList, View } from "react-native";
 import { AppSearchBar } from "../../components/AppSearchBar/AppSearchBar";
 import { Text } from "@rneui/base";
 import { AppOfferCard } from "../../components/AppOfferCard/AppOfferCard";
+import { useEffect, useState } from "react";
 
 export default () => {
   const styles = { ...AppStyles(), ...AccueilStyles() };
 
   const handleEndEditing = (value) => console.log(value);
 
-  const data = [
-    {
-      id: 1,
-      titre: "Première offre",
-      etat: "neuf",
-      prix: "99",
-      uri: "https://placehold.co/300x400/png",
-    },
-    {
-      id: 2,
-      titre: "Deuxième offre",
-      etat: "Bon etat",
-      prix: "77,00",
-      uri: "https://placehold.co/600x300/png",
-    },
-    {
-      id: 3,
-      titre: "Troisième offre",
-      etat: "Mauvais état",
-      prix: "1,00",
-      uri: "https://placehold.co/300x600/png",
-    },
-    {
-      id: 4,
-      titre: "Quatrième offre",
-      etat: "neuf",
-      prix: "9,99",
-      uri: "https://placehold.co/50x50/png",
-    },
-  ];
+  // const offres = [
+  //   {
+  //     id: 1,
+  //     titre: "Première offre",
+  //     descrition: "Description Première offre",
+  //     etat: "neuf",
+  //     prix: "99",
+  //     photos: "https://placehold.co/300x400/png",
+  //   },
+  //   {
+  //     id: 2,
+  //     titre: "Deuxième offre",
+  //     descrition: "Description Deuxième offre",
+  //     etat: "Bon etat",
+  //     prix: "77,00",
+  //     photos: "https://placehold.co/600x300/png",
+  //   },
+  //   {
+  //     id: 3,
+  //     titre: "Troisième offre",
+  //     descrition: "Description Troisième offre",
+  //     etat: "Mauvais état",
+  //     prix: "1,00",
+  //     photos: "https://placehold.co/300x600/png",
+  //   },
+  //   {
+  //     id: 4,
+  //     titre: "Quatrième offre",
+  //     descrition: "Description Quatrième offre",
+  //     etat: "neuf",
+  //     prix: "9,99",
+  //     photos: "https://placehold.co/50x50/png",
+  //   },
+  // ];
+
+  const [offres, setOffres] = useState([]);
+
+  async function fetchOffres() {
+    try {
+      const response = await fetch("http://192.168.43.137:4000/offres");
+      if (!response.ok) {
+        throw new Error("La récupération des offres a échoué");
+      }
+      const offres = await response.json();
+      return offres;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchOffres().then(setOffres);
+  }, []);
 
   return (
     <View style={[styles.container, styles.safeArea]}>
@@ -50,7 +74,7 @@ export default () => {
         </Text>
 
         <FlatList
-          data={data}
+          data={offres}
           horizontal={true}
           renderItem={({ item }) => (
             <AppOfferCard
@@ -58,10 +82,10 @@ export default () => {
               titre={item.titre}
               etat={item.etat}
               prix={item.prix}
-              uri={item.uri}
+              uri={item.photos[0]}
             ></AppOfferCard>
           )}
-          keyExtractor={(item) => item.id}
+          keyExtractor={(item) => item._id}
         />
       </View>
     </View>
